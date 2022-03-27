511604dc15e56bd9e3a10197ae134f0dc6f9e79f
ChiTung Fok <chitungfok@gmail.com>
2022-03-27 16:44:04 +0800

    add

---
+++ findMedianSortedArrays/findMedianSortedArrays.go
@@ -0,0 +1,41 @@
+package main
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var (
+		index1 int
+		index2 int
+		res    []int
+	)
+	for {
+		if index1 == len(nums1) && index2 == len(nums2) {
+			break
+		}
+		if index1 == len(nums1) {
+			res = append(res, nums2[index2:]...)
+			break
+		}
+		if index2 == len(nums2) {
+			res = append(res, nums1[index1:]...)
+			break
+		}
+		if nums1[index1] < nums2[index2] {
+			res = append(res, nums1[index1])
+			index1++
+			continue
+		}
+		res = append(res, nums2[index2])
+		index2++
+	}
+	if (len(nums1)+len(nums2))%2 > 0 {
+		mid := (len(nums1) + len(nums2)) / 2
+		return float64(res[mid])
+	}
+	mid := (len(nums1) + len(nums2)) / 2
+	return (float64(res[mid-1]) + float64(res[mid])) / 2
+}
+
+func main() {
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+	findMedianSortedArrays(nums1, nums2)
+}
